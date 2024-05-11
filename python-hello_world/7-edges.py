61ea271cfc568c67fce1dd65985fe76867f40577
‘Krisha <8805@holbertonstudents.com>
2024-05-11 11:16:19 +1000

    Slicing and indexing a string using F-strings

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = 'Holberton'[:3]
-word_last_2 = 'Holberton'[-2:]
-middle_word = 'Holberton'[1:-1]
-print(f"First 3 letters: {word_first_3}")
-print(f"Last 2 letters: {word_last_2}")
-print(f"Middle word: {middle_word}")
+
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
+ 
+#word_first_3 = 'Holberton'[:3]
+#word_last_2 = 'Holberton'[-2:]
+#middle_word = 'Holberton'[1:-1]
+#print(f"First 3 letters: {word_first_3}")
+#print(f"Last 2 letters: {word_last_2}")
+# print(f"Middle word: {middle_word}")
