ef1b7e7bcec3cac3adffa154d603126ab5db39a1
‘Krisha <8805@holbertonstudents.com>
2024-05-16 13:57:33 +1000

    Program that prints the number of and the list of its arguments

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import sys
+
+if __name__ == "__main__":
+    num_args = len(sys.argv)
+
+    if num_args == 1:
+        print("No arguments passed.")
+    elif num_args == 2:
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
+    else:
+        print("{} arguments:".format(num_args - 1))
+        for i in range(1, num_args):
+            print("{}: {}".format(i, sys.argv[i]))
