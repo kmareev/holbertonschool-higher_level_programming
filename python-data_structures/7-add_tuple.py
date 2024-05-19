05395ca8467623bf7cef084aadc9ec180af2f7d0
‘Krisha <8805@holbertonstudents.com>
2024-05-19 18:43:24 +1000

    Function that adds 2 tupples

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -4,6 +4,6 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Ensure each tuple has at least 2 elements by appending zeros if necessary
     tuple_a += (0, 0)
     tuple_b += (0, 0)
-    
+
     # Create a new tuple by adding the first two elements of each tuple
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
