3b709b10cb408fbb0b7bda4fbb76107d9dc445c1
‘Krisha <8805@holbertonstudents.com>
2024-05-22 09:50:57 +1000

    Returns a new dict with all values multiplied by 2

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,7 +3,7 @@
 def multiply_by_2(a_dictionary):
     new_dict = {}
 
-    #iterate over the key-value pairs in dict
+    # iterate over the key-value pairs in dict
     for key, value in a_dictionary.items():
         new_dict[key] = value * 2
     return new_dict
