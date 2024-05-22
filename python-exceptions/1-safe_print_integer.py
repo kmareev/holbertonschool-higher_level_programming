12235b1806189677ecdea625d0ee8bd1e50db00a
‘Krisha <8805@holbertonstudents.com>
2024-05-22 14:10:27 +1000

    Prints an integer value using "{:d}".format() while handling exceptions using a try-except block

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,9 @@
 
 def safe_print_integer(value):
     try:
+        if isinstance(value, int):  # checks if value is an integer
         print("{:d}".format(value))
         return True
     
-    except ValueError:
+    except (TypeError ValueError):
         return False
