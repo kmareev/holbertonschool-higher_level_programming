426793f5fa7c0e80ef289d7b4f077422d2ce6385
‘Krisha <8805@holbertonstudents.com>
2024-05-22 13:56:19 +1000

    Prints the first x elements of a list, handling cases where x maybe larger than the length of the list using a try-except block

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    # initializes variable count to 0
+    count = 0
+    
+    try: # try-except block then loop over elements
+        for element in my_list[:x]:
+            print(element, end='')
+            count += 1
+    except IndexError:
+        pass
+    print() # print newline character
+    return count
