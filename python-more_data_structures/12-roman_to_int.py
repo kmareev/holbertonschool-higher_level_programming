87e8ec527f18058d6e00b82d2d7816b4c37ae96d
‘Krisha <8805@holbertonstudents.com>
2024-05-22 12:57:19 +1000

    Converts a roman numeral to integer

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) or roman_string is None:
         return 0
-    
+
     roman_num = {
         'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50,
         'C' : 100, 'D' : 500, 'M' : 1000
@@ -15,7 +15,7 @@ def roman_to_int(roman_string):
     for c in roman_string:
         if c not in roman_num:
             return 0
-        
+
         current_value = roman_num[c]
 
         if current_value > prev_value:
