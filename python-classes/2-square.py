ec66d158355138331feceecbe0b58771da3096c8
‘Krisha <8805@holbertonstudents.com>
2024-05-26 21:05:03 +1000

    Fixed pycodestyle

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 '''This module defines a Square class.'''
 
 class Square:
@@ -14,13 +15,10 @@ class Square:
             TypeError: if size is not an int.
             ValueError: if size is less than 0.
         '''
-        
+
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
 
         self.__size = size
-
-    
-
