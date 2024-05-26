8599f8ed4d4566556b9d0b9b02892a9371b3af76
‘Krisha <8805@holbertonstudents.com>
2024-05-26 21:09:14 +1000

    Fixed pycodestyle

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,9 @@
 #!//usr/bin/python3
-'''This module defines a Square class.'''
+"""This module defines a Square class."""
 
 
 class Square:
-    '''Represents a square.'''
+    """Represents a square."""
 
     def __init__(self) -> None:
         pass
