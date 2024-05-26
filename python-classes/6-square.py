15b2b1d2c1b174551625d211b4b85b85ad558362
‘Krisha <8805@holbertonstudents.com>
2024-05-26 22:39:39 +1000

    Implementation of a square class, including setters and getters of size and positiion, area calculation and printing visual representation of the square

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -66,5 +66,8 @@ class Square:
         if self.size == 0:
             print()
         else:
+            for _ in range(self.__position[1]):
+                print("")
             for _ in range(self.size):
                 print("#" * self.size)
+                print(" " * self.__position[0], end="")
