f86c8ef0944b9da124c242c6f0cd657b473031dd
‘Krisha <8805@holbertonstudents.com>
2024-05-26 22:06:34 +1000

    Added my_print method to print the square with the char #/ If the eize is 0 it will print an empty line

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -5,27 +5,24 @@
 class Square:
     """Represents a square."""
 
-
     def __init__(self, size=0):
-      """Initializes a new Square instance.
-
+        """Initializes a new Square instance.
         Args:
             size: The size of the square.
         """
-      self.__size = size
+      self.size = size
 
     @property
     def size(self):
       """Getter for the current size of the Square."""
-
       return self.__size
     
     @size.setter
     def size(self, value):
        """Setter for the current size of the Square.
-            Raises:
-                TypeError: if size is not an int.
-                ValueError: if size is less than 0.
+        Raises:
+            TypeError: if size is not an int.
+            ValueError: if size is less than 0.
        """
        if not isinstance (value, int):
           raise TypeError ("size must be an integer")
