76b3ebaf97d93fcb2f07da13688d0fd137e2b40d
‘Krisha <8805@holbertonstudents.com>
2024-05-27 22:55:51 +1000

    Run tests

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def print_square(size):
+    """
+    Prints a square with the character #.
+
+    Args:
+        size (int): The size length of the square.
+
+    Raises:
+        TypeError: If size is not an integer or if size is a float less than 0.
+        ValueError: If size is less than 0.
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for _ in range(size):
+        print("#" * size)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+    """
+    Prints a text with 2 new lines after each of these characters: ., ? and :
+
+    Args:
+        text (str): The text to be processed.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    characters = {'.', '?', ':'}
+    result = ""
+    i = 0
+
+    while i < len(text):
+        result += text[i]
+        if text[i] in characters:
+            result += "\n\n"
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+            continue
+        i += 1
+
+    print(result, end='')
