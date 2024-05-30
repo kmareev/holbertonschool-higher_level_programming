6fddc0edf7af9b474e7472366486427605573b67
‘Krisha <8805@holbertonstudents.com>
2024-05-30 23:50:30 +1000

    Validates if value is an integer

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -23,6 +23,6 @@ class BaseGeometry:
             ValueError: If value is less than or equal to 0
         """
         if not isinstance(value, int):
-            raise TypeError(f"<name> must be an integer")
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError(f"<name> must be greater than 0")
+            raise ValueError(f"{name} must be greater than 0")
