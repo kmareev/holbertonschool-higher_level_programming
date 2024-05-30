4ace6d1b04901441529744ec1df0e0249fc101e2
‘Krisha <8805@holbertonstudents.com>
2024-05-30 16:28:01 +1000

    Function that prints first and last name

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """The module that prints first and last name"""
 
+
 def say_my_name(first_name, last_name=""):
     """
     Prints a statement with the given first name and last name.
@@ -16,5 +17,5 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-    
+
     print(f"My name is {first_name} {last_name}")
