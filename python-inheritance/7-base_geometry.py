208143dae755d28302e73bfb83144ca173d80b3a
‘Krisha <8805@holbertonstudents.com>
2024-05-30 22:36:25 +1000

    Validates if value is an integer

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""This is an empty BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """An empty class caleld BaseGeomatry."""
+
+    def area(self):
+        """Area not implemented."""
+        raise Exception("area() is not implemented")
+    
+    def integer_validator(self, name, value):
+        """
+        Validates the value to ensure it is a positive integer
+
+        Args:
+            name (str): The name of the value
+            value (int): The value to validate
+
+        Raises:
+            TypeError: If value is not an integer
+            ValueError: If value is less than or equal to 0
+        """
+        if not isinstance(value, int):
+            raise TypeError(f"<name> must be an integer")
+        if value <= 0:
+            raise ValueError(f"<name> must be greater than 0")
