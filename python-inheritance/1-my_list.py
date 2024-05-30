428a03955b7529ef9b35b250fd17b748b2e615ed
‘Krisha <8805@holbertonstudents.com>
2024-05-30 21:57:36 +1000

    Create a class MyList that inherits from list

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,6 +2,7 @@
 """This module defines the MyList class, with a functionality
 to print the list in ascending order"""
 
+
 class MyList(list):
     """A subclass of list with an additional method to
     print the list sorted"""
