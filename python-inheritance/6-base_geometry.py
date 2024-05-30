4a43aafd975c9fc597af994f6e25cf8f893631cb
‘Krisha <8805@holbertonstudents.com>
2024-05-30 22:27:34 +1000

    Raises an Exception message when area is called

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -5,7 +5,7 @@
 
 class BaseGeometry:
     """An empty class caleld BaseGeomatry."""
-    
+
     def area(self):
         """Area not implemented."""
         raise Exception("area() is not implemented")
