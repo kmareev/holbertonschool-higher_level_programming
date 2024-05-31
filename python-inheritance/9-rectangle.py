20898eefe3a331b579030b462abb61f0b71d2582
‘Krisha <8805@holbertonstudents.com>
2024-05-31 16:22:30 +1000

    Defined area method to calculate area of a rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This is base class for geometry operation.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A class representing a Rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Calculates the area of a rectangle.
+        Returns:
+            int area of a rectangle."""
+        return self.__width * self.__height
