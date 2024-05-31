7a7fd7801dbd094358ef45ee8339ff55c9a950a2
‘Krisha <8805@holbertonstudents.com>
2024-05-31 16:10:47 +1000

    Created a Rectangle class that inherits from BaseGeometry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""This is base class for geometry operation.
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A ckass representing a Rectangle."""
+
+    def __init__(self, width, height):
+        """Initialize rectangle with width and height."""
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
