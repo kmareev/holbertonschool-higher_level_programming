aec5fc9b596f1a0fa067476e1196ef1fbc82354f
‘Krisha <8805@holbertonstudents.com>
2024-05-31 16:41:32 +1000

    Created a Square subclass that inherits from Rectangle

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -4,6 +4,7 @@ Rectangle base class."""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
     """A subclass of Rectangle representing
     a Square."""
@@ -13,13 +14,13 @@ class Square(Rectangle):
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
-    
+
     def area(self):
         """Calculates the area of a Square.
         Returns:
             int area of the square."""
         return self.__size * self.__size
-    
+
     def __str__(self):
         """Return the string representation of the square."""
         return f"[Square] {self.__size}/{self.__size}"
