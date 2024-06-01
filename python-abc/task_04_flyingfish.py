0bb8fd22e2efac7becdb2e689057da45293dc556
‘Krisha <8805@holbertonstudents.com>
2024-06-01 21:40:34 +1000

    Uses multiple inheritance to inherit attributes and behaviours from more than one parent class

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""This module uses multiple inheritance."""
+
+class Fish:
+    """A class representing a Fish"""
+    def swim(self):
+        print("The fish is swimming")
+    def habitat(self):
+        print("The fish lives in water")
+    
+class Bird:
+    """A class representing a Bird"""
+    def fly(self):
+        print("The bird is flying")
+    def habitat(self):
+        print("The bird lives in the sky")
+    
+class FlyingFish(Fish, Bird):
+    """A class representing a FlyingFish,
+    inheriting from Fish and Bird, known as multiple
+    inheritance."""
+
+    def fly(self):
+        print("The flying fish is soaring!")
+
+    def swim(self):
+        print("The flying fish is swimming!")
+
+    def habitat(self):
+        print("The flying fish lives in both water and the sky!")
+    
