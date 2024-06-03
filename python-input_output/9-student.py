b77136dcfd6664218ea36c5ecf5182c2d946706c
‘Krisha <8805@holbertonstudents.com>
2024-06-03 23:51:56 +1000

    Retrieves a dict representation of a Student instance

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-"""Implementation of a function called
-'student'"""
+"""In this implementation, the __init__ method initializes
+the first_name, last_name, and age attributes, and the to_json
+method returns a dictionary representation of a Student instance with
+these attributes."""
 
 
 class Student:
@@ -9,9 +11,9 @@ and age."""
     def __init__(self, first_name, last_name, age):
         """Initializes method"""
         self.first_name = first_name
-        self.last_name - last_name
+        self.last_name = last_name
         self.age = age
 
-        def to_json(self):
-            """Retrieves a dict representation of a Student instance"""
-            return self.__dict__
+    def to_json(self):
+        """Retrieves a dict representation of a Student instance"""
+        return self.__dict__
