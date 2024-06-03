5099de29bcc3ff7aa04e56590559170f87b4365e
‘Krisha <8805@holbertonstudents.com>
2024-06-03 23:44:45 +1000

    Returns a dictionary description with simple data structure or JSON serialization of an object

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""The implementation of a function
+'class_to_json."""
+
+def class_to_json(obj):
+    """Returns a dictionary description with simple data structure
+for JSON serialization of an object"""
+    return obj.__dict__
