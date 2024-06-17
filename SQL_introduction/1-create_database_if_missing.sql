ac15a53d761d37307bc85b962c116604386bd3e1
‘Krisha <8805@holbertonstudents.com>
2024-06-17 17:30:29 +1000

    SQL query to display all rows in a table

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,4 @@
 --Creates the database hbtn_0c_0 in your MySQL server
 --If database already exists, my script should not fail
 -- Not allowed to use the SELECT or SHOW statements
-CREATE DATABASE hbtn_0c_0 IF NOT exist;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
