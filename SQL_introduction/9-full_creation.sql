63aa2dae818b0ff8fa8924f49239fbe415b0523b
‘Krisha <8805@holbertonstudents.com>
2024-06-20 12:51:20 +1000

    Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,4 +1,4 @@
---Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
+-- Creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT NOT NULL,
     name VARCHAR(256),
