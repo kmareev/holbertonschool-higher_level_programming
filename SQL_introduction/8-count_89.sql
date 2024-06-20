a58b44fbbaef362689e13b0885ec6b1da35354c8
‘Krisha <8805@holbertonstudents.com>
2024-06-20 12:50:31 +1000

    Displays the no of records ith id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,2 @@
---Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+-- Displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
 SELECT COUNT(*) AS record_count FROM first_table WHERE id = 89;
