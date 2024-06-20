8fb89eeeaa74b8724bc03ab7985acf4f0418e35c
‘Krisha <8805@holbertonstudents.com>
2024-06-20 12:52:29 +1000

    Prints score and names in order, with top score first

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,4 +1,4 @@
---Print both the score and the name of second_table, in order
---record should be ordered by score (top first)
+-- Print both the score and the name of second_table, in order
+-- Record should be ordered by score (top first)
 
 SELECT score, name FROM second_table  ORDER BY score DESC;
