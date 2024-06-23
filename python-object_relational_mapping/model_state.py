1c011e1748c1136086bab0cb49bdd4d491f86926
‘Krisha <8805@holbertonstudents.com>
2024-06-23 23:10:21 +1000

    Fixed line erorr

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -8,10 +8,10 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
     """Representing the base class of a State in the database
     """
     __tablename__ = 'states'
-
     id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
