785f91a3522a1aeb4ba0d5795bb73e58d52d13e6
‘Krisha <8805@holbertonstudents.com>
2024-06-23 22:29:38 +1000

    Created a python file that contains the class definition of a State and an instance Base = declarative_base()

---
+++ SQL_introduction/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""A  python file that contains the class definition
+of a State and an instance Base = declarative_base()
+"""
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    """Representing the base class of a State in the database
+    """
+    __tablename__ = 'states'
+
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
