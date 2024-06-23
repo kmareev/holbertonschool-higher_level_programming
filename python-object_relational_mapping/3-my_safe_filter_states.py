73643c694a3999706408ddc5f70507e10f6ef50e
‘Krisha <8805@holbertonstudents.com>
2024-06-23 17:00:30 +1000

    Protects database from MySQL injections

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module with the same function as 2-my_filter_states however,
+this one is safe from MySQL injections!"""
+
+if __name__ == "__main__":
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    mycursor = db.cursor()
+
+    mycursor.execute("""SELECT * FROM states
+                     WHERE BINARY NAME = %s
+                     ORDER BY id ASC""".format(sys.argv[4]))
+    
+    for x in mycursor:
+        print(x)
+    
