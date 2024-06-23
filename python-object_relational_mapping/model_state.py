31355b1673380505017d1ef5ed09bc82b20afc26
‘Krisha <8805@holbertonstudents.com>
2024-06-23 22:52:36 +1000

    removed file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -3,8 +3,8 @@
 of a State and an instance Base = declarative_base()
 """
 
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
