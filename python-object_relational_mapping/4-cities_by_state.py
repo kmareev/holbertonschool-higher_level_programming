9570c031d57fe7783cbfc6e2ca50fd4483ac58a2
‘Krisha <8805@holbertonstudents.com>
2024-06-23 17:28:29 +1000

    List all cities and their states using INNER JOIN function

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     query = """SELECT cities.id, cities.name, states.name
                      FROM cities
                      INNER JOIN states ON cities.state_id = states.id"""
-    
+
     mycursor.execute(query)
 
     rows = mycursor.fetchall()
