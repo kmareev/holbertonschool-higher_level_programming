aa7ad9d6dcf6fa55a969d611ecd345338b5c4d52
‘Krisha <8805@holbertonstudents.com>
2024-06-25 23:16:54 +1000

    Adds the State object “Louisiana” to the database hbtn_0e_6_usa

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    new_state = State(name='Louisana')
+    new_state = State(name='Louisiana')
     session.add(new_state)
     session.commit()
 
