2ec02564691c54efef88a6f3c6b14456d42af330
‘Krisha <8805@holbertonstudents.com>
2024-06-25 22:04:23 +1000

    Lists all State objects that contain the letter a from the database hbtn_0e_6_usa

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""Lists all State objects that contain the letter a from the database hbtn_0e_6_usa
+"""Lists all State objects that contain the
+letter 'a' from the database hbtn_0e_6_usa
 """
-
+# Import necessary modules and functions
 from sys import argv
 from model_state import Base, State
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-# Import necessary modules and functions
 if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
 
 # Create a connection string w/ username, pw and db name
+# Establish a connection to db
     connect_str = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
 
-# Establish a connection to db
+
 # Create a session to interact with db
     engine = create_engine(connect_str, pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
