3dc2dc35937278599e5aa6cdb3c0ebbfe45dd28e
‘Krisha <8805@holbertonstudents.com>
2024-06-30 20:34:48 +1000

    Updates the name of a State from a database

---
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that changes the name of a State object from the
+database hbtn_0e_6_usa
+"""
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    user = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    connect_str = f"mysql+mysqldb://{user}:{passwd}@localhost:3306/{db}"
+
+    engine = create_engine(connect_str, pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    update_state = session.query(State).filter(State.id == 2).first()
+
+    if update_state:
+        update_state.name = "New Mexico"
+        session.commit()
+
+    session.close()
