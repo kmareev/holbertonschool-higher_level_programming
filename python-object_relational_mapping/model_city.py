cd161954b5adcaec08677a7e19ef6160b3c949e3
‘Krisha <8805@holbertonstudents.com>
2024-06-30 21:19:52 +1000

    Prints all cities from the database hbtn_0e_14_usa

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -2,6 +2,7 @@
 """A python file that contains a class definition of a
 City an instance base = declarative_base()"""
 
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import State, Base
